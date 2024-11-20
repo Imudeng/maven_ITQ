071f080f432386c963250cbeb53992ce705a3805
Igor Vodin <ivodin93@gmail.com>
2024-11-20 16:55:32 +0400

    сохранимка2

--- Maven1/src/test/java/tests_collection_1/MassiveTests.java
+++ Maven1/src/test/java/tests_collection_1/MassiveTests.java
@@ -7,34 +7,39 @@ import org.testng.annotations.*;
 import java.util.Arrays;
 import java.util.List;
 
-public class MassiveTests {
+class MassiveTests {
     List<String> userList = Arrays.asList("Cat", "Dog", "Parrot");
+
     @BeforeClass
-    public void beforeClass () {
+    public void beforeClass() {
         System.out.println("Test classa Massiv");
     }
+
     @BeforeTest
-    public void beforeFirst (){
+    public void beforeFirst() {
         System.out.println("Nachnem 1 test potom poidut sleduychie");
     }
 
     @BeforeMethod
-    public void beforeMethod () {
+    public void beforeMethod() {
         System.out.println("Nachali test");
     }
+
     @AfterMethod
-    public void afterMethod () {
+    public void afterMethod() {
         System.out.println("Zakochili test");
     }
 
     @AfterClass
-    public void afterClass () {
+    public void afterClass() {
         System.out.println("Ito bil test classa Massiv. Kakoi sleduychi test?");
     }
+
     @AfterTest
-    public void afterThird (){
+    public void afterThird() {
         System.out.println("Zakonchili 3 test");
     }
+
     @Test(description = "Optimalnoe kolichetvo pitomzev")
     public void testEquals() {
         int actual = 3;
@@ -62,39 +67,6 @@ public class MassiveTests {
     }
 
 }
-public class NumberTests {
-    int userNumber = 5;
 
-    @Test
-    public void testEquals() {
-        int actual = 5;
-        Assert.assertEquals(userNumber, actual, "Ravenstvo ne podtverzdeno");
-        System.out.println("class NumberTests " + "method testEquals");
-    }
-
-    @Test
-    public void testCompare() {
-        boolean expected = true;
-        boolean actual = isCompare(userNumber);
-        Assert.assertEquals(expected, actual, "Chislo menshe 10");
-        System.out.println("class NumberTests " + "method testCompare");
-    }
-
-    private boolean isCompare(int number) {
-        return number > 10;
-    }
-
-    @Test
-    public void testChet() {
-        boolean expected = true;
-        boolean actual = isChet(userNumber);
-        Assert.assertEquals(expected, actual, "Chislo nechetnoe");
-        System.out.println("class NumberTests " + "method testChet");
-    }
-
-    private boolean isChet(int number) {
-        return number % 2 == 0;
-    }
-}
 
 
